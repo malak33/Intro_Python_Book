4d917f004f18d02ec8d55ddd3e0b611b5a57a168
Michael Nobile <nobility33@gmail.com>
2015-04-19 21:59:47 -0400

    second bulk add

--- convert.py
+++ convert.py
@@ -1,10 +1,10 @@
-#convert.py
+# Convert.py
 # A program to convert Celsius temps to Fahrenheit
 # by: Susan Computewell
 
 def main():
     celsius = eval(input("What is the Celsius temperature? "))
     fahrenheit = 9/5 * celsius + 32
-    print("The temperature is", fahrenheit, "degrees Fahrenheit.")
-    
+    print("The temperature is", fahrenheit, "degrees fahrenheit.")
+
 main()
--- quadratic.py
+++ quadratic.py
@@ -1,21 +1,17 @@
 #quadratic.py
-# A program that computes the real roots of a quadratic equation.
-# Illustrates use of the math library.
-# Note: this program crashes if the equation has no real roots
+# a program that computes the real roots of a quadratic equation.
+# note: this program crashes if the equation has no real roots.
 
-import math # makes the math library available.
-
+import math
 def main():
-    print("This program finds the real solutions to a quadratic")
-    print()
-    
+    print("This program finds the real solutions to a quadratic\n")
+
     a, b, c = eval(input("Please enter the coefficients (a, b, c): "))
-    
+
     discRoot = math.sqrt(b * b - 4 * a * c)
     root1 = (-b + discRoot) / (2 * a)
     root2 = (-b - discRoot) / (2 * a)
-    
-    print()
-    print("The solutions are:", root1, root2)
-    
+
+    print("\nThe solutions are:", root1, root2 )
+
 main()
